870d2a9cfd4898a696e7af167d38cb6018887ff1
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-01-23 10:32:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt upgrade -y ;
 
 #
 sudo apt install -y git curl htop net-tools emacs-nox lame unar axel vlc gedit build-essential ffmpeg imagemagick ;
-sudo apt install -y chromium-browser ;
+sudo apt install -y chromium ;
 #sudo apt install -y handbrake ;
 #sudo apt install -y inetutils-ping ;
 #sudo apt install -y kdenlive ;
@@ -14,7 +14,7 @@ sudo apt install -y chromium-browser ;
 #
 sudo apt install -y fonts-takao-pgothic fonts-takao-gothic fonts-takao-mincho ;
 sudo apt install -y locales ;
-sudo dpkg-reconfigure locales ;
+#sudo dpkg-reconfigure locales ;
 export LANG=ja_JP.UTF-8 ;
 echo "export LANG=ja_JP.UTF-8" >> ~/.profile ;
 
