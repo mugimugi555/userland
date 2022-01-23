cd33765a730c28bcd676b23839af26afa82e44ca
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-01-23 16:30:35 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,22 +6,14 @@ sudo apt upgrade -y ;
 
 #
 sudo apt install -y git curl htop net-tools emacs-nox lame unar axel vlc gedit build-essential ffmpeg imagemagick ;
-sudo apt install -y chromium ;
 #sudo apt install -y handbrake ;
 #sudo apt install -y inetutils-ping ;
 #sudo apt install -y kdenlive ;
 
 #
-#sudo apt install -y fonts-takao-pgothic fonts-takao-gothic fonts-takao-mincho ;
-#sudo apt install -y locales ;
-#sudo dpkg-reconfigure locales ;
-#export LANG=ja_JP.UTF-8 ;
-#echo "export LANG=ja_JP.UTF-8" >> ~/.profile ;
-
-#
-sudo rm /etc/localtime ;
-sudo echo Asia/Tokyo > /etc/timezone ;
-sudo dpkg-reconfigure -f noninteractive tzdata ;
+sudo apt install -y chromium ;
+echo "alias chrome='chromium --no-sandbox'" >> ~/.profile
+source ~/.profile
 
 #
 sudo apt install -y gimp ;
@@ -31,3 +23,18 @@ sudo apt install -y libcanberra-gtk* ;
 #
 sudo apt install -y python3-pip ;
 sudo -H pip install --upgrade youtube-dl ;
+
+#
+sudo rm /etc/localtime ;
+sudo echo Asia/Tokyo > /etc/timezone ;
+sudo dpkg-reconfigure -f noninteractive tzdata ;
+
+#
+sudo apt install -y fonts-takao-pgothic fonts-takao-gothic fonts-takao-mincho ;
+sudo apt install -y locales ;
+sudo dpkg-reconfigure locales ;
+export LANG=ja_JP.UTF-8 ;
+echo "export LANG=ja_JP.UTF-8" >> ~/.profile ;
+
+#
+echo "please reboot userland app";
