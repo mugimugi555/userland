62d7591859d196ce46dde4aa40b5174ab8f3bcb0
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-01-28 08:09:38 +0900

    Create install_laravel.sh

---
+++ install_laravel.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# wget https://raw.githubusercontent.com/mugimugi555/userland/main/install_laravel.sh && bash install_laravel.sh ;
+
+cd;
+sudo echo;
+sudo apt install -y apache2 php php-xml php-bcmath php-mbstring php-xml php-zip mariadb-server ;
+
+#
+php -r "copy ( 'https://getcomposer.org/installer', 'composer-setup.php' ) ;" ;
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer ;
+composer -v ;
+
+#
+sudo mv /var/www/html/ /var/www/html_back ;
+mkdir ~/html ;
+sudo ln -s /home/$USER/html/ /var/www/html ;
+echo "hi" > ~/html/index.html ;
+
+#
+composer global require laravel/installer ;
+PATH="$PATH:$HOME/bin:$HOME/.config/composer/vendor/bin" ;
+source .profile ;
+laravel new testproject ;
+sudo chown www-data:www-data -R testproject/storage testproject/bootstrap/cache ;
+ln -s /home/$USER/testproject/public /var/www/html/testproject ;
