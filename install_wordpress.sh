5dceab0c2d1a27e393ece00f8288200576f66f2a
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-01-29 11:41:33 +0900

    Update install_wordpress.sh

--- install_wordpress.sh
+++ install_wordpress.sh
@@ -14,10 +14,10 @@ unzip latest-ja.zip ;
 
 #
 echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | sudo mysql ;
-echo "GRANT ALL ON wordpress.* TO wordpress@localhost IDENTIFIED BY 'mypassword';" | sudo mysql ;
-echo "FLUSH PRIVILEGES;" | sudo mysql ;
+echo "GRANT ALL ON wordpress.* TO wordpress@localhost IDENTIFIED BY 'mypassword';"   | sudo mysql ;
+echo "FLUSH PRIVILEGES;"                                                             | sudo mysql ;
 
 #
 echo "http://localhost:8080/wordpress/" ;
-echo "user_id  => wordpress " ;
+echo "user_id  => wordpress  " ;
 echo "password => mypassword " ;
