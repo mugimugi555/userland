63f6a045c30b80971652e899815df638ed151af7
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-01-31 12:47:20 +0900

    Update install_wordpress.sh

--- ubuntu/install_wordpress.sh
+++ ubuntu/install_wordpress.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/bash
 
+# wget https://raw.githubusercontent.com/mugimugi555/userland/main/ubuntu/install_wordpress.sh && install_wordpress.sh ;
+
+#
 sudo apt install -y unzip ;
 
+#
 cd /var/www/html ;
 wget https://ja.wordpress.org/latest-ja.zip ;
 unzip latest-ja.zip ;
 
+#
 echo "SHOW DATABASES;" | sudo mysql -uroot ;
-echo "SHOW DATABASES;" | sudo mysql -uroot ;
-echo "SHOW DATABASES;" | sudo mysql -uroot ;
-
 echo "CREATE DATABASE wordpress ;"                                   | sudo mysql -uroot ;
 echo "CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'password';" | sudo mysql -uroot ;
-echo "GRANT ALL ON wordpress.* to 'wordpress'@'localhost';"          | sudo mysql -uroot ;
-
+echo "GRANT ALL ON wordpress.* TO 'wordpress'@'localhost';"          | sudo mysql -uroot ;
 echo "SHOW DATABASES;"                    | sudo mysql -uroot ;
 echo "SELECT user, host FROM mysql.user;" | sudo mysql -uroot ;
 
+#
 LOCAL_IPADDRESS=`hostname -I | awk -F" " '{print $1}'` ;
 echo $LOCAL_IPADDRESS ;
-
 echo "visit => http://$LOCAL_IPADDRESS:8080/wordpress/" ;
 echo "ID       => wordpress" ;
 echo "PASSWORD => password"  ;
