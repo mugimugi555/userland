d316a1ec88f01bad43d73b6fba9c751175166357
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-01-31 12:56:55 +0900

    Update install_wordpress.sh

--- install_wordpress.sh
+++ install_wordpress.sh
@@ -4,20 +4,25 @@
 
 #
 sudo apt install -y unzip ;
+# sudo apt install -y apache2 php php-mysql mariadb-server ;
 
 #
 cd /var/www/html ;
 wget https://ja.wordpress.org/latest-ja.zip ;
 unzip latest-ja.zip ;
 
-# sudo apt install apache2 php php-mysql-y  ;
+#
+echo "SHOW DATABASES;"                                                               | sudo mysql -uroot ;
+echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | sudo mysql -uroot ;
+echo "CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'password';"                 | sudo mysql -uroot ;
+echo "GRANT ALL ON wordpress.* TO 'wordpress'@'localhost';"                          | sudo mysql -uroot ;
+echo "SHOW DATABASES;"                                                               | sudo mysql -uroot ;
+echo "SELECT user, host FROM mysql.user;"                                            | sudo mysql -uroot ;
 
 #
-echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | sudo mysql ;
-echo "GRANT ALL ON wordpress.* TO wordpress@localhost IDENTIFIED BY 'mypassword';"   | sudo mysql ;
-echo "FLUSH PRIVILEGES;"                                                             | sudo mysql ;
-
-#
-echo "http://localhost:8080/wordpress/" ;
-echo "user_id  => wordpress  " ;
-echo "password => mypassword " ;
+LOCAL_IPADDRESS=`hostname -I | awk -F" " '{print $1}'` ;
+echo $LOCAL_IPADDRESS ;
+echo "visit => http://$LOCAL_IPADDRESS:8080/wordpress/" ;
+echo "ID       => wordpress" ;
+echo "PASSWORD => password"  ;
+echo "DB name  => wordpress" ;
