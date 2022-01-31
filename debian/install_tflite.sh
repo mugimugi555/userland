66a4148693680142f22d2a0dc58d4aad6a491ee9
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-01-31 13:31:48 +0900

    Update and rename install_tflite.sh to debian/install_tflite.sh

--- debian/install_tflite.sh
+++ debian/install_tflite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# wget https://raw.githubusercontent.com/mugimugi555/userland/main/install_tflite.sh && bash install_tflite.sh ;
+# wget https://raw.githubusercontent.com/mugimugi555/userland/main/debian/install_tflite.sh && bash install_tflite.sh ;
 
 #
 sudo apt update ;
