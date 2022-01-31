241d1603df7ce04cc46cd98355582c286e3d4ab9
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-01-31 14:25:16 +0900

    Update install_lamp.sh

--- debian/install_lamp.sh
+++ debian/install_lamp.sh
@@ -4,8 +4,18 @@
 
 sudo apt install -y apache2 php php-mysql mariadb-server ;
 
-sudo sed -e 's/80/8080/' /etc/apache2/ports.conf ;
+sudo sed -i '/Listen/{s/\([0-9]\+\)/8080/; :a;n; ba}' /etc/apache2/ports.conf
 
+#
 sudo echo "export LD_LIBRARY_PATH=\"\"" >> /support/startVNCServerStep2.sh ;
-sudo echo "/usr/sbin/apachectl start" >> /support/startVNCServerStep2.sh ;
-sudo echo "sudo service mysql start" >> /support/startVNCServerStep2.sh ;
+sudo echo "/usr/sbin/apachectl start"   >> /support/startVNCServerStep2.sh ;
+sudo echo "sudo service mysql start"    >> /support/startVNCServerStep2.sh ;
+
+#
+sudo service mysql start ;
+/usr/sbin/apachectl start ;
+
+#
+LOCAL_IPADDRESS=`hostname -I | awk -F" " '{print $1}'` ;
+echo $LOCAL_IPADDRESS ;
+echo "visit => http://$LOCAL_IPADDRESS:8080/" ;
