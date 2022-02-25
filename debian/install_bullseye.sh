2eb0bba4e9459dae101c3d8f16eaab6f3c656c5f
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-02-25 10:50:47 +0900

    Create install_bullseye.sh

---
+++ debian/install_bullseye.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+# wget
+
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.back ;
+
+NEWSOURCELIST=$(cat<<TEXT
+deb http://ftp.jp.debian.org/debian/ bullseye main contrib non-free
+deb-src http://ftp.jp.debian.org/debian/ bullseye main contrib non-free
+
+deb http://ftp.jp.debian.org/debian/ bullseye-updates main contrib non-free
+deb-src http://ftp.jp.debian.org/debian/ bullseye-updates main contrib non-free
+
+deb http://security.debian.org/ bullseye-security main
+deb-src http://security.debian.org/ bullseye-security main
+TEXT
+)
+sudo echo "$NEWSOURCELIST" | sudo tee /etc/apt/sources.list ;
+
+sudo apt update ;
+sudo apt upgrade -y ;
+sudo apt full-upgrade -y ;
+# apt upgrade --without-new-pkgs
