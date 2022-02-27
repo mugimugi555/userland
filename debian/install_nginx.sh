547ca62e776a56e4ba39e39bbcd208b28a794852
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-02-27 10:09:01 +0900

    Update install_nginx.sh

--- debian/install_nginx.sh
+++ debian/install_nginx.sh
@@ -1,16 +1,28 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-# sudo apt update ; sudo apt install -y wget ; wget https://raw.githubusercontent.com/mugimugi555/userland/main/debian/install_nginx.sh && bash install_nginx.sh ;
+# ======================================================================================================================
+# one liner command
+# ======================================================================================================================
+# sudo apt update -y ; sudo apt install -y wget ; wget https://raw.githubusercontent.com/mugimugi555/userland/main/debian/install_nginx.sh && bash install_nginx.sh ;
 
+# ======================================================================================================================
+# add nginx key
+# ======================================================================================================================
 wget http://nginx.org/keys/nginx_signing.key ;
 sudo apt-key add nginx_signing.key ;
 
+# ======================================================================================================================
+# add repository
+# ======================================================================================================================
 sudo touch /etc/apt/sources.list.d/nginx.list ;
 VERSION_CODENAME=$(env -i bash -c '. /etc/os-release; echo $VERSION_CODENAME') ;
 echo $VERSION_CODENAME ;
-echo "deb http://nginx.org/packages/debian/ $VERSION_CODENAME nginx" | sudo tee /etc/apt/sources.list.d/nginx.list ;
+echo "deb http://nginx.org/packages/debian/ $VERSION_CODENAME nginx"     | sudo tee /etc/apt/sources.list.d/nginx.list ;
 echo "deb-src http://nginx.org/packages/debian/ $VERSION_CODENAME nginx" | sudo tee -a /etc/apt/sources.list.d/nginx.list ;
 cat /etc/apt/sources.list.d/nginx.list ;
 
+# ======================================================================================================================
+# install nginx
+# ======================================================================================================================
 sudo apt update ;
 sudo apt install -y nginx ;
