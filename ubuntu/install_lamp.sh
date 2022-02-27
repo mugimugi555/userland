433bc61dde938439e0be5c49dcbabdf4c581626a
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-02-27 10:18:58 +0900

    Update install_lamp.sh

--- ubuntu/install_lamp.sh
+++ ubuntu/install_lamp.sh
@@ -1,28 +1,48 @@
 #!/usr/bin/bash
 
+# ======================================================================================================================
+# one liner command
+# ======================================================================================================================
 # wget https://raw.githubusercontent.com/mugimugi555/userland/main/ubuntu/install_lamp.sh && bash install_lamp.sh ;
 
-#
+# ======================================================================================================================
+# install common and add repository
+# ======================================================================================================================
 sudo apt install -y software-properties-common ;
 sudo add-apt-repository -y ppa:ondrej/php ;
+
+# ======================================================================================================================
+# install php mysql apache
+# ======================================================================================================================
 sudo apt install -y apache2 libapache2-mod-php8.1 php8.1 mariadb-server ;
 sudo apt install -y php8.1-{mysql,imap,ldap,xml,curl,mbstring,zip} ;
 php -v ;
 #sudo a2dismod php7.4 ;
 #sudo a2enmod php8.1 ;
 #sudo update-alternatives --config php ;
+
+# ======================================================================================================================
+# set port number
+# ======================================================================================================================
 sudo sed -i '/Listen/{s/\([0-9]\+\)/8080/; :a;n; ba}' /etc/apache2/ports.conf
 
-#
-sudo echo "export LD_LIBRARY_PATH=\"\"" >> /support/startVNCServerStep2.sh ;
-sudo echo "/usr/sbin/apachectl start"   >> /support/startVNCServerStep2.sh ;
-sudo echo "sudo service mysql start"    >> /support/startVNCServerStep2.sh ;
+# ======================================================================================================================
+# start auto
+# ======================================================================================================================
+echo "export LD_LIBRARY_PATH=\"\"" | sudo tee -a /support/startVNCServerStep2.sh ;
+echo "/usr/sbin/apachectl start"   | sudo tee -a /support/startVNCServerStep2.sh ;
+echo "sudo service mysql start"    | sudo tee -a /support/startVNCServerStep2.sh ;
 
-#
+# ======================================================================================================================
+# start manually
+# ======================================================================================================================
 sudo service mysql start ;
 /usr/sbin/apachectl start ;
 
-#
+# ======================================================================================================================
+# finish
+# ======================================================================================================================
 LOCAL_IPADDRESS=`hostname -I | awk -F" " '{print $1}'` ;
-echo $LOCAL_IPADDRESS ;
+echo "======================================" ;
 echo "visit => http://$LOCAL_IPADDRESS:8080/" ;
+echo "======================================" ;
