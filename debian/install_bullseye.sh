01baf48d9c6866cfc2ecdc4699ab60cb8332163d
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-02-27 10:08:34 +0900

    Update install_bullseye.sh

--- debian/install_bullseye.sh
+++ debian/install_bullseye.sh
@@ -1,9 +1,18 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-# sudo apt update ; sudo apt install -y wget ; wget https://raw.githubusercontent.com/mugimugi555/userland/main/debian/install_bullseye.sh && bash install_bullseye.sh ;
+# ======================================================================================================================
+# one liner command
+# ======================================================================================================================
+# sudo apt update -y ; sudo apt install -y wget ; wget https://raw.githubusercontent.com/mugimugi555/userland/main/debian/install_bullseye.sh && bash install_bullseye.sh ;
 
+# ======================================================================================================================
+# backup
+# ======================================================================================================================
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.back ;
 
+# ======================================================================================================================
+# replace repository
+# ======================================================================================================================
 NEWSOURCELIST=$(cat<<TEXT
 deb http://ftp.jp.debian.org/debian/ bullseye main contrib non-free
 deb-src http://ftp.jp.debian.org/debian/ bullseye main contrib non-free
@@ -17,6 +26,9 @@ TEXT
 )
 sudo echo "$NEWSOURCELIST" | sudo tee /etc/apt/sources.list ;
 
+# ======================================================================================================================
+# update
+# ======================================================================================================================
 sudo apt update ;
 sudo apt upgrade -y ;
 sudo apt full-upgrade -y ;
