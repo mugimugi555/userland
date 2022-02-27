21efd39c8addf70f429b917db3e4a9e571de482f
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-02-27 10:09:32 +0900

    Update install_php81.sh

--- debian/install_php81.sh
+++ debian/install_php81.sh
@@ -1,12 +1,20 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-# sudo apt update ; sudo apt install -y wget ; wget https://raw.githubusercontent.com/mugimugi555/userland/main/debian/install_php81.sh && bash install_php81.sh ;
+# ======================================================================================================================
+# one liner command
+# ======================================================================================================================
+# sudo apt update -y ; sudo apt install -y wget ; wget https://raw.githubusercontent.com/mugimugi555/userland/main/debian/install_php81.sh && bash install_php81.sh ;
 
+# ======================================================================================================================
+# add repository
+# ======================================================================================================================
 VERSION_CODENAME=$(env -i bash -c '. /etc/os-release; echo $VERSION_CODENAME') ;
-
 sudo apt install -y gnupg2 ca-certificates apt-transport-https software-properties-common ;
 wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add - ;
 echo "deb https://packages.sury.org/php/ $VERSION_CODENAME main" | sudo tee /etc/apt/sources.list.d/php.list ;
 
+# ======================================================================================================================
+# install php
+# ======================================================================================================================
 sudo apt update ;
 sudo apt install -y php8.1 php8.1-fpm ;
