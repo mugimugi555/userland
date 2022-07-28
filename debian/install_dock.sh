ca56f3fe2125c01bb7e36986082d91bbb889feef
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-29 00:21:38 +0900

    Update install_dock.sh

--- debian/install_dock.sh
+++ debian/install_dock.sh
@@ -3,6 +3,7 @@
 # wget https://github.com/mugimugi555/userland/raw/main/debian/install_dock.sh && bash install_dock.sh ;
 
 # https://linuxhint.com/custom-dock-xfce/
+sudo apt install -y gnupg2 ca-certificates apt-transport-https software-properties-common ;
 sudo add-apt-repository ppa:xubuntu-dev/extras ;
 sudo apt update ;
 sudo apt install -y xfce4-docklike-plugin ;
