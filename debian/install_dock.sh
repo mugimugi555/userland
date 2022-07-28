bf09e157a2366fcaa34597e7b8f7074f43f6deb0
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-28 21:32:20 +0900

    Update install_dock.sh

--- debian/install_dock.sh
+++ debian/install_dock.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+# wget https://github.com/mugimugi555/userland/raw/main/debian/install_dock.sh && bash install_dock.sh ;
+
 # https://linuxhint.com/custom-dock-xfce/
 sudo add-apt-repository ppa:xubuntu-dev/extras ;
 sudo apt update ;
