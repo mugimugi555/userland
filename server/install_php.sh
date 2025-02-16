a05e914376d8fafb49d9ccae3839315d6190caeb
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2025-02-16 16:39:07 +0900

    Update install_php.sh

--- server/install_php.sh
+++ server/install_php.sh
@@ -34,12 +34,13 @@ echo "📌 最新の PHP バージョン: PHP $PHP_VERSION をインストール
 # PHP のインストール
 # ======================================================================
 echo "📌 PHP $PHP_VERSION をインストール中..."
-sudo apt install -y "php$PHP_VERSION" "php$PHP_VERSION-cli" "php$PHP_VERSION-fpm" "php$PHP_VERSION-common" \
-                    "php$PHP_VERSION-curl" "php$PHP_VERSION-mbstring" "php$PHP_VERSION-xml" "php$PHP_VERSION-zip" \
-                    "php$PHP_VERSION-gd" "php$PHP_VERSION-mysql" "php$PHP_VERSION-pgsql" "php$PHP_VERSION-sqlite3" \
-                    "php$PHP_VERSION-bcmath" "php$PHP_VERSION-intl" "php$PHP_VERSION-soap" "php$PHP_VERSION-readline" \
-                    "php$PHP_VERSION-opcache" "php$PHP_VERSION-xmlrpc" "php$PHP_VERSION-redis" "php$PHP_VERSION-imagick" \
-                    "php$PHP_VERSION-dev" "php$PHP_VERSION-ldap"
+sudo apt install -y \
+    "php$PHP_VERSION"         "php$PHP_VERSION-cli"      "php$PHP_VERSION-fpm"   "php$PHP_VERSION-common"   \
+    "php$PHP_VERSION-curl"    "php$PHP_VERSION-mbstring" "php$PHP_VERSION-xml"   "php$PHP_VERSION-zip"      \
+    "php$PHP_VERSION-gd"      "php$PHP_VERSION-mysql"    "php$PHP_VERSION-pgsql" "php$PHP_VERSION-sqlite3"  \
+    "php$PHP_VERSION-bcmath"  "php$PHP_VERSION-intl"     "php$PHP_VERSION-soap"  "php$PHP_VERSION-readline" \
+    "php$PHP_VERSION-opcache" "php$PHP_VERSION-xmlrpc"   "php$PHP_VERSION-redis" "php$PHP_VERSION-imagick"  \
+    "php$PHP_VERSION-dev"     "php$PHP_VERSION-ldap"
 
 # ======================================================================
 # PHP のバージョン確認
